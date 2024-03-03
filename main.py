d03499b04e7e45f2c81c703f8342d0ae9d44eed8
Alexandr <alex.pathfree@yandex.by>
2024-03-03 23:07:39 +0300

    Добавлено вращение User-Agent

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import random
+
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    # добавь больше User-Agent, если нужно
+]
 
 
 def get_html(url):
+    headers = {'User-Agent': random.choice(USER_AGENTS)}
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
     except requests.RequestException as e:
         print(f"Произошла ошибка при отправке запроса: {e}")
